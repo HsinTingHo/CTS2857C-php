a0dc09eb0ba6e6c01832469e6991ab6a24e5f729
HsinTingHo <yama762682@gmail.com>
2018-03-18 13:07:53 -0400

    assignment note in index.php

---
+++ index.php
@@ -0,0 +1,8 @@
+<?php
+/*
+1. 5 PHP pages with links to all the other pages.
+	on one of the page, a form that prompts for an email address with a submit button that will send the number of times the pages have been visited to the entered email address.
+2. Each time a page is visited, add 1 to a counter.
+3. display the number of times the page has been visited and list the number of times the other pages have been visited.
+*/
+?>
